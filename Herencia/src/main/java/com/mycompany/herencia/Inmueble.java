8131b729b1839c477b44ef5730f9b9fb956ade31
Samuel <samuelbeltran048@gmail.com>
2025-04-02 14:24:36 -0500

    Se realizo la clase Inmueble y se modifico la clase Herencia

---
+++ Herencia/src/main/java/com/mycompany/herencia/Inmueble.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.herencia;
+
+
+public class Inmueble {
+     
+    protected int identificadorinmobiliario;
+    protected int area;
+    protected String direccion;
+    protected double precioVenta;
+
+    public Inmueble(int identificadorinmobiliario, int area, String direccion) {
+        this.identificadorinmobiliario = identificadorinmobiliario;
+        this.area = area;
+        this.direccion = direccion;
+    }
+    
+    public void calcularPrecioVenta(double valorArea){
+        
+    }
+    
+    public void imprimir(){
+        
+    }
+}
+
